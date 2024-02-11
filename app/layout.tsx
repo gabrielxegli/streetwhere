68e18c9b0bef1bf525380c94cc13740bccd2ef66
Gabriel Egli <gabriel.j.egli+git@gmail.com>
2024-02-12 00:18:15 +0100

    changed splashscreen

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,41 +66,56 @@ export const metadata: Metadata = {
     statusBarStyle: "default",
     title: APP_DEFAULT_TITLE,
     startupImage: [
-      {
-        url: "/splash-screen?w=2048&h=2732",
-        media:
-          "(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)",
-      },
-      {
-        url: "/splash-screen?w=1668&h=2224",
-        media:
-          "(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)",
-      },
-      {
-        url: "/splash-screen?w=1536&h=2048",
-        media:
-          "(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)",
-      },
-      {
-        url: "/splash-screen?w=1125&h=2436",
-        media:
-          "(device-width: 375px) and (device-height: 812px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: portrait)",
-      },
-      {
-        url: "/splash-screen?w=1242&h=2148",
-        media:
-          "(device-width: 414px) and (device-height: 736px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: portrait)",
-      },
-      {
-        url: "/splash-screen?w=750&h=1294",
-        media:
-          "(device-width: 375px) and (device-height: 667px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)",
-      },
-      {
-        url: "/splash-screen?w=640&h=1136",
-        media:
-          "(device-width: 320px) and (device-height: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)",
-      },
+      ...[
+        { w: 2048, h: 2732, x: 2 },
+        { w: 1668, h: 2388, x: 2 },
+        { w: 1668, h: 2388, x: 2 },
+        { w: 1536, h: 2048, x: 2 },
+        { w: 1488, h: 2266, x: 2 },
+        { w: 1536, h: 2048, x: 2 },
+        { w: 1640, h: 2360, x: 2 },
+        { w: 1668, h: 2224, x: 2 },
+        { w: 1536, h: 2048, x: 2 },
+        { w: 1620, h: 2160, x: 2 },
+        { w: 1536, h: 2048, x: 2 },
+        { w: 1290, h: 2796, x: 3 },
+        { w: 1179, h: 2556, x: 3 },
+        { w: 1290, h: 2796, x: 3 },
+        { w: 1179, h: 2556, x: 3 },
+        { w: 1290, h: 2796, x: 3 },
+        { w: 1179, h: 2556, x: 3 },
+        { w: 1284, h: 2778, x: 3 },
+        { w: 1170, h: 2532, x: 3 },
+        { w: 1284, h: 2778, x: 3 },
+        { w: 1170, h: 2532, x: 3 },
+        { w: 1125, h: 2436, x: 3 },
+        { w: 1284, h: 2778, x: 3 },
+        { w: 1170, h: 2532, x: 3 },
+        { w: 1170, h: 2532, x: 3 },
+        { w: 1125, h: 2436, x: 3 },
+        { w: 1242, h: 2688, x: 3 },
+        { w: 1125, h: 2436, x: 3 },
+        { w: 828, h: 1792, x: 2 },
+        { w: 1242, h: 2688, x: 3 },
+        { w: 1125, h: 2436, x: 3 },
+        { w: 828, h: 1792, x: 2 },
+        { w: 1125, h: 2436, x: 3 },
+        { w: 1080, h: 1920, x: 3 },
+        { w: 750, h: 1334, x: 2 },
+        { w: 1080, h: 1920, x: 3 },
+        { w: 750, h: 1334, x: 2 },
+        { w: 1080, h: 1920, x: 3 },
+        { w: 750, h: 1334, x: 2 },
+        { w: 1080, h: 1920, x: 3 },
+        { w: 750, h: 1334, x: 2 },
+        { w: 750, h: 1334, x: 2 },
+        { w: 750, h: 1334, x: 2 },
+        { w: 640, h: 1136, x: 2 },
+        { w: 640, h: 1136, x: 2 },
+      ].map(({ w, h, x }) => ({
+        url: `/splash-screen?w=${w}&h=${h}`,
+        media: `(device-width: ${w}px) and (device-height: ${h}px) and (-webkit-min-device-pixel-ratio: ${x})`,
+      })),
     ],
   },
 
