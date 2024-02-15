ab81489142ee687247784d8c6133702fa026649f
Gabriel Egli <gabriel.j.egli+git@gmail.com>
2024-02-15 12:17:24 +0100

    fixed issue yess

--- app/incoming-mails/route.ts
+++ app/incoming-mails/route.ts
@@ -85,12 +85,3 @@ export async function POST(req: Request) {
 
   return new Response("ok", { status: 200 });
 }
-
-function base64ToArrayBuffer(base64: string): ArrayBuffer {
-  var binaryString = atob(base64);
-  var bytes = new Uint8Array(binaryString.length);
-  for (var i = 0; i < binaryString.length; i++) {
-    bytes[i] = binaryString.charCodeAt(i);
-  }
-  return bytes.buffer;
-}
